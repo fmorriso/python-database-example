f9bfcbdb92a9a6c51f671f6d871bb1e0d6585909
Frederick Morrison <frederick.morrison@outlook.com>
2023-07-28 15:47:28 -0600

    add connection, cursor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, sqlite3
 
 
 def get_python_version() -> str:
@@ -8,3 +8,11 @@ def get_python_version() -> str:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(f'Python version {get_python_version()}')
+
+    print(sqlite3.version)
+
+    connection : sqlite3.Connection = sqlite3.connect(":memory:")
+    print(connection)
+
+    cursor: sqlite3.Cursor = connection.cursor()
+    print(cursor)
